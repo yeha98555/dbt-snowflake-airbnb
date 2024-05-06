32e4e01d24b148ac2dc4655c6473ac97273d7d05
yeha98555 <yeha98555@gmail.com>
2024-05-06 03:40:11 +0000

    feat: Add dim models

---
+++ dbtlearn/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,16 @@
+WITH src_hosts AS (
+    SELECT * FROM {{ ref("src_hosts") }}
+)
+
+SELECT
+    host_id,
+    COALESCE(
+        NULLIF(host_name, ''), 
+        'Anonymous'
+    ) AS host_name,
+    is_superhost,
+    created_at,
+    updated_at
+FROM
+    src_hosts
+
