c97c4a123e40ebe0a0287970d9f3c979e55c24f8
yeha98555 <yeha98555@gmail.com>
2024-06-22 12:06:09 +0000

    build: Add poetry to devcontainer

--- scripts/setup_codespace_env.sh
+++ scripts/setup_codespace_env.sh
@@ -3,6 +3,17 @@ set -x
 
 mkdir -p ~/.dbt
 ln -sf $(readlink -f profiles.yml) ~/.dbt/profiles.yml
-pip3 install --user -r requirements.txt
+
+pip3 install poetry==1.2.2
+
+# Convenience workspace directory for later use
+WORKSPACE_DIR=$(pwd)
+
+# Change some Poetry settings to better deal with working in a container
+poetry config cache-dir ${WORKSPACE_DIR}/.cache
+poetry config virtualenvs.in-project true
+
+# Now install all dependencies
+poetry install
 
 # code profiles.yml
